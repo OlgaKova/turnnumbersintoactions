c294b7b090c49c86d42ff695416338d8c74d0bcf
OlgaKova <olga.kavaleuskaya@gmail.com>
2015-07-22 23:48:29 -0700

    changed count to sum quanitity to make it correct

--- sql/Counting the number of items per order.sql
+++ sql/Counting the number of items per order.sql
@@ -1,5 +1,5 @@
 /*Counting the number of items per order*/
-SELECT sfo.increment_id, COUNT( item_id ) items_number
+SELECT sfo.increment_id, sum( qty_ordered ) items_number
 FROM  `sales_flat_order_item` AS sfoi
 INNER JOIN  `sales_flat_order` AS sfo ON sfo.entity_id = sfoi.order_id
 GROUP BY order_id
